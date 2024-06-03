857c1a7f1f466e1d5fc87f0644610f4903ddae23
toirovashabbona <toirovashabbona167@gmail.com>
2024-06-03 08:17:25 +0500

    Add files via upload

---
+++ 3dars/index.js
@@ -0,0 +1,33 @@
+// var takePlace = prompt("Moshinalar")
+//     switch (takePlace){
+
+//         case "Nexia ":
+//         case "Malibu":
+//         case "Cobalt":
+//         case "Damas":
+//         case "Matiz":
+//         case "Spark":
+//         document.write("O'zbekiston moshinalari");
+//         break
+//         case "Mers":
+//         case "BWN":
+//         case "Porsche":
+//         document.write("Germaniya moshinalari");
+//         break
+//         default:
+//     }
+    
+// var num1 = +prompt("Birinchi sonni kiriting");
+// var math = +prompt("Amalni ")
+
+
+
+
+
+
+
+        
+
+
+    
+
